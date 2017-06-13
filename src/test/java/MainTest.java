396814bb0db375b90a047636f38066d81ac30137
Eirik Skjærseth <eirik1502@gmail.com>
2017-06-13 09:48:05 +0200

    add .gitignore, add a test class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by eirik on 12.06.2017.
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() {
+
+        Main m = new Main();
+        Assert.assertEquals(m.toString(), "main");
+    }
+}
