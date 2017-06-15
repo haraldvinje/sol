8e8b4d0a876b07b6151bec52d22f5df28d345fde
Eirik <eirik1502@gmail.com>
2017-06-15 17:29:19 +0200

    add rotation comp

---
+++ src/main/java/engine/RotationComp.java
@@ -0,0 +1,25 @@
+package engine;
+
+/**
+ * Created by eirik on 15.06.2017.
+ */
+public class RotationComp implements Component {
+
+    private float angle;
+
+
+    public RotationComp() {
+
+    }
+    public RotationComp(float angle) {
+        this.angle = angle;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+}
