59689a25c8c150ca6e0cf920afc0e7d8a3106275
Eirik <eirik1502@gmail.com>
2017-06-19 15:02:14 +0200

    add mass, frictionConst, frictionModel, elsticity and impulse

--- src/main/java/engine/physics/PhysicsComp.java
+++ src/main/java/engine/physics/PhysicsComp.java
@@ -8,49 +8,105 @@ import utils.maths.Vec2;
  */
 public class PhysicsComp implements Component {
 
+
+    public final static float DEFAULT_MASS = 50.0f;
+    public final static float DEFAULT_FRICTION_CONST = 0.5f;
+    public final static float DEFAULT_ELASTICITY = 0.7f;
+    public static final int DEFAULT_FRICTION_MODEL = PhysicsUtil.FRICTION_MODEL_COULOMB;
+
+
     private Vec2 velocity = new Vec2();
     private Vec2 acceleration = new Vec2();
+    private Vec2 impulse = new Vec2();
 
-    private float frictionConstant = 0.1f;
+    private int frictionModel;
+    private float invMass = 1f;
+    private float frictionConst = 0.5f;
+    private float elasticity = 1;
 
 
 
-    public void setFrictionConstant(float frictionConstant){
-        this.frictionConstant = frictionConstant;
+    public PhysicsComp(float mass, float frictionConst, float elasticity, int frictionModel) {
+        setMass(mass);
+        setFrictionConstant(frictionConst);
+        setElasticity(elasticity);
+        setFrictionModel(frictionModel);
+    }
+    public PhysicsComp(float mass, float frictionConst, float elasticity) {
+        this(mass,frictionConst, elasticity, DEFAULT_FRICTION_MODEL);
+    }
+    public PhysicsComp(float mass, float frictionConst) {
+        this(mass, frictionConst, DEFAULT_ELASTICITY, DEFAULT_FRICTION_MODEL);
+    }
+    public PhysicsComp(float mass) {
+        this(mass, DEFAULT_FRICTION_CONST, DEFAULT_ELASTICITY, DEFAULT_FRICTION_MODEL);
+    }
+    public PhysicsComp() {
+        this(DEFAULT_MASS, DEFAULT_FRICTION_CONST, DEFAULT_ELASTICITY, DEFAULT_FRICTION_MODEL);
     }
 
-    public void addVelocity(Vec2 velocity) {
-        this.velocity = this.velocity.add(velocity);
-        //System.out.println("Add physics velocity" + this.velocity);
-    }
-
-//    public void setVelocity(Vec2 vector){
-//        this.velocity = vector;
-//        this.vx = vector.x;
-//        this.vy = vector.y;
-//    }
 
     public Vec2 getVelocity(){
-        return this.velocity;
+            return this.velocity;
+        }
+    public void addVelocity(Vec2 velocity) {
+        this.velocity = this.velocity.add(velocity);
     }
+    public Vec2 getImpulse() {
+        return impulse;
+    }
+    public void addImpulse(Vec2 impulse) {
+        this.impulse = this.impulse.add(impulse);
+    }
+    public void resetImpulse() {
+        impulse.setZero();
+    }
+
 
     public Vec2 getAcceleration() {
-        return acceleration;
-    }
-
-//    public void setAcceleration(Vec2 acceleration) {
-//        this.acceleration = acceleration;
-//    }
-
-    public float getFrictionConst(){
-        return this.frictionConstant;
-    }
-
+            return acceleration;
+        }
     public void addAcceleration(Vec2 acceleration){
         this.acceleration = this.acceleration.add(acceleration);
     }
-
     public void resetAcceleration() {
-        this.acceleration = new Vec2();
+        this.acceleration.setZero();
+    }
+
+
+    public void setFrictionConstant(float frictionConstant){
+        this.frictionConst = frictionConstant;
+    }
+    public float getFrictionConst(){
+            return this.frictionConst;
+        }
+
+    public float getMass() {
+        if (invMass == 0) {
+            return 0;
+        }
+        return 1/invMass;
+    }
+    public float getInvMass() {
+        return invMass;
+    }
+    public void setMass(float m) {
+        if (m == 0)
+            invMass = 0;
+        else
+            invMass = 1/m;
+    }
+    public float getElasticity() {
+        return elasticity;
+    }
+    public void setElasticity(float elasticity) {
+        this.elasticity = elasticity;
+    }
+
+    public int getFrictionModel() {
+        return frictionModel;
+    }
+    public void setFrictionModel(int frictionModel) {
+        this.frictionModel = frictionModel;
     }
 }
