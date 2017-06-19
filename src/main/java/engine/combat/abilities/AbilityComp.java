92d4433a96faaf5948fcf4a03cb1fc6ef592faf4
Eirik <eirik1502@gmail.com>
2017-06-19 17:39:07 +0200

    made classes for ability and damage systems. Will implement tomorrow

---
+++ src/main/java/engine/combat/abilities/AbilityComp.java
@@ -0,0 +1,21 @@
+package engine.combat.abilities;
+
+import engine.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eirik on 19.06.2017.
+ */
+public class AbilityComp implements Component {
+
+    private static final int EXPECTED_MELEE_ABILITIES = 2;
+    private static final int EXPECTED_PROJECTILE_ABILITIES = 2;
+
+    private List<MeleeAbility> meleeAbilities = new ArrayList<>(EXPECTED_MELEE_ABILITIES);
+    private List<ProjectileAbility> projectileAbilities = new ArrayList<>(EXPECTED_PROJECTILE_ABILITIES);
+
+
+
+}
