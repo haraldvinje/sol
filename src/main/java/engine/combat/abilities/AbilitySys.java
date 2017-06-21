47ead8ab7dff648ea46979cb3b5b9e5f6665f86b
haraldvinje <haraldvinje@gmai.com>
2017-06-21 14:21:24 +0200

    added abilitySys to. Attaches hitbox to relevant entity

--- src/main/java/engine/combat/abilities/AbilitySys.java
+++ src/main/java/engine/combat/abilities/AbilitySys.java
@@ -1,7 +1,47 @@
 package engine.combat.abilities;
 
+import engine.PositionComp;
+import engine.RotationComp;
+import engine.Sys;
+import engine.WorldContainer;
+import utils.maths.Vec2;
+
+import java.util.Set;
+
 /**
  * Created by haraldvinje on 21-Jun-17.
  */
-public class AbilitySys {
+public class AbilitySys implements Sys {
+
+    WorldContainer worldContainer;
+
+    @Override
+    public void setWorldContainer(WorldContainer wc) {
+        this.worldContainer = wc;
+    }
+
+    @Override
+    public void update() {
+        Set<Integer> abilityComps = worldContainer.getEntitiesWithComponentType(AbilityComp.class);
+        for (int entity: abilityComps){
+            AbilityComp abComp = (AbilityComp) worldContainer.getComponent(entity, AbilityComp.class);
+            PositionComp posComp = (PositionComp) worldContainer.getComponent(entity, PositionComp.class);
+            RotationComp rotComp = (RotationComp) worldContainer.getComponent(entity, RotationComp.class);
+            float x = posComp.getX();
+            float y = posComp.getY();
+            float angle = rotComp.getAngle();
+            for (MeleeAbility meleeAbility: abComp.getMeleeAbilities()){
+
+                int hitBoxEntity = meleeAbility.getHitBoxEntity();
+
+                PositionComp mPosComp = (PositionComp) worldContainer.getComponent(hitBoxEntity, PositionComp.class);
+
+                Vec2 vector = Vec2.newLenDir(meleeAbility.getRelativeDistance(), angle);
+                Vec2 result = new Vec2(x,y).add(vector);
+                mPosComp.setPos(result);
+
+            }
+
+        }
+    }
 }
