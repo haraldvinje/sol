0245fb18efb2e4cb68700dcfeaf697e217d1db9d
haraldvinje <haraldvinje@gmai.com>
2017-06-22 20:49:39 +0200

    restructured abilitySys to be more readable

--- src/main/java/engine/combat/abilities/AbilitySys.java
+++ src/main/java/engine/combat/abilities/AbilitySys.java
@@ -25,63 +25,92 @@ public class AbilitySys implements Sys {
 
     @Override
     public void update() {
-        Set<Integer> abilityComps = worldContainer.getEntitiesWithComponentType(AbilityComp.class);
-        for (int entity: abilityComps){
+        Set<Integer> abilityCompEntities = worldContainer.getEntitiesWithComponentType(AbilityComp.class);
 
+        for (int entity: abilityCompEntities){
             AbilityComp abComp = (AbilityComp) worldContainer.getComponent(entity, AbilityComp.class);
 
-            if (abComp.isOccupied()){
-                continue;
-            }
 
             PositionComp posComp = (PositionComp) worldContainer.getComponent(entity, PositionComp.class);
             RotationComp rotComp = (RotationComp) worldContainer.getComponent(entity, RotationComp.class);
 
-            float x = posComp.getX();
-            float y = posComp.getY();
-            float angle = rotComp.getAngle();
-
             for (MeleeAbility meleeAbility: abComp.getMeleeAbilities()){
-                int hitboxEntity = meleeAbility.getHitboxEntity();
-                PhysicsComp phComp = (PhysicsComp)(worldContainer.getComponent(hitboxEntity, PhysicsComp.class));
-                PositionComp mPosComp = (PositionComp) (worldContainer.getComponent(hitboxEntity, PositionComp.class));
-
-                int startupTime = meleeAbility.getStartupTime();
-                int activeHitboxTime = meleeAbility.getActiveHitboxTime();
-                int endingLagTime = meleeAbility.getEndlagTime();
-                int attackDurationTime = startupTime + activeHitboxTime + endingLagTime;
-                int rechargeTime = meleeAbility.getRechargeTime();
-
-                phComp.setVelocity(new Vec2(0,0));
-                if (meleeAbility.isExecuting()) {
-                    abComp.setOccupied(true);
-                    meleeAbility.counter++;
-                    if (startupTime + activeHitboxTime>meleeAbility.counter && meleeAbility.counter>=startupTime){
-                        meleeAbility.setActiveHitbox(true);
-                        Vec2 vector = Vec2.newLenDir(meleeAbility.getRelativeDistance(), angle);
-                        Vec2 result = new Vec2(x, y).add(vector);
-                        mPosComp.setPos(result);
-                        System.out.println("During hitbox");
-
-                    }
-                    else if (attackDurationTime>meleeAbility.counter && meleeAbility.counter>=startupTime + activeHitboxTime){
-                        meleeAbility.setActiveHitbox(false);
-                        System.out.println("During ending lag");
-                    }
-                    else if (attackDurationTime + rechargeTime>meleeAbility.counter && meleeAbility.counter>=attackDurationTime){
-                        abComp.setOccupied(false);
-                        System.out.println("During recharge phase");
-                    }
-                    else if (meleeAbility.counter>=attackDurationTime+rechargeTime) {
-                        meleeAbility.setExecuting(false);
-                        System.out.println("Now you can execute attack again :)");
-                    }
-                    else{
-                        System.out.println("In startup frames");
-                    }
-                }
+                updateMeleeAbility(meleeAbility, abComp, posComp, rotComp);
             }
-
         }
     }
+
+    private void updateMeleeAbility(MeleeAbility meleeAbility, AbilityComp abComp, PositionComp playerPosComp, RotationComp playerRotComp){
+
+        int hitboxEntity = meleeAbility.getHitboxEntity();
+
+        PhysicsComp meleePhComp = (PhysicsComp)(worldContainer.getComponent(hitboxEntity, PhysicsComp.class));
+        PositionComp meleePosComp = (PositionComp) (worldContainer.getComponent(hitboxEntity, PositionComp.class));
+
+        int startupTime = meleeAbility.getStartupTime();
+        int activeHitboxTime = meleeAbility.getActiveHitboxTime();
+        int endingLagTime = meleeAbility.getEndlagTime();
+        int attackDurationTime = startupTime + activeHitboxTime + endingLagTime;
+        int rechargeTime = meleeAbility.getRechargeTime();
+
+        meleePhComp.resetVelocity();
+
+        if (meleeAbility.isExecuting()) {
+            //if not occupied
+            abComp.setOccupied(true);
+            meleeAbility.counter++;
+
+            if (meleeAbility.counter == startupTime) {
+                activateHitbox(meleeAbility);
+            }
+
+            else if (startupTime + activeHitboxTime > meleeAbility.counter){
+                duringActiveHitbox(meleeAbility, meleePosComp, playerPosComp, playerRotComp);
+
+            }
+
+            else if (startupTime + activeHitboxTime == meleeAbility.counter){
+                duringEndingLag(meleeAbility);
+            }
+
+            else if (attackDurationTime == meleeAbility.counter){
+                duringRecharge(abComp);
+            }
+            else if (attackDurationTime+rechargeTime == meleeAbility.counter) {
+                afterRecharge(meleeAbility);
+            }
+            else{
+            }
+        }
+    }
+
+
+
+    private void activateHitbox(MeleeAbility meleeAbility){
+        meleeAbility.setActiveHitbox(true);
+    }
+
+
+    private void duringActiveHitbox(MeleeAbility meleeAbility, PositionComp meleePosComp, PositionComp playerPosComp, RotationComp playerRotCom ){
+        Vec2 vector = Vec2.newLenDir(meleeAbility.getRelativeDistance(), playerRotCom.getAngle());
+        Vec2 result = playerPosComp.getPos().add(vector);
+
+        meleePosComp.setPos(result);
+
+    }
+
+    private void duringEndingLag(MeleeAbility meleeAbility){
+        meleeAbility.setActiveHitbox(false);
+    }
+
+    private void duringRecharge(AbilityComp abComp) {
+        abComp.setOccupied(false);
+    }
+
+    private void afterRecharge(MeleeAbility meleeAbility){
+        meleeAbility.setExecuting(false);
+    }
+
+
+
 }
