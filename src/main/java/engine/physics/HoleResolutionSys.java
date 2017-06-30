0350266a76937e3cfc3c3a84a80b43ff6fb5df54
Eirik <eirik1502@gmail.com>
2017-06-30 11:08:55 +0200

    change hole resolution to only set a flag in holAffectedComp. What happens on hole collision is determined elsewhere

--- src/main/java/engine/physics/HoleResolutionSys.java
+++ src/main/java/engine/physics/HoleResolutionSys.java
@@ -22,6 +22,13 @@ public class HoleResolutionSys implements Sys {
 
     @Override
     public void update() {
+
+        worldContainer.entitiesOfComponentTypeStream(AffectedByHoleComp.class).forEach( affholeEntity -> {
+            //reset affected flag
+            AffectedByHoleComp affholeComp = (AffectedByHoleComp) worldContainer.getComponent(affholeEntity, AffectedByHoleComp.class);
+            affholeComp.resetHoleAffectedFlag();
+        });
+
         Set<Integer> holeEntities = worldContainer.getEntitiesWithComponentType(HoleComp.class);
         int otherEntity = -1;
         for (int entity: holeEntities){
@@ -54,18 +61,22 @@ public class HoleResolutionSys implements Sys {
 
     }
 
-    private void onCollision(CollisionData data, int entity) {
-        respawnEntities(data, entity);
+    private void onCollision(CollisionData data, int affectedEntity) {
+        AffectedByHoleComp affholeComp = (AffectedByHoleComp) worldContainer.getComponent(affectedEntity, AffectedByHoleComp.class);
+
+        affholeComp.setHoleAffectedFlag();
+
+//        respawnEntities(data, entity);
     }
 
-    private void respawnEntities(CollisionData data, int entity){
-        //TODO: Make this method much more general
-        PositionComp posComp = (PositionComp) worldContainer.getComponent(entity, PositionComp.class);
-
-        posComp.setPos(new Vec2(300,450));
-
-        ((DamageableComp)worldContainer.getComponent(entity, DamageableComp.class)).reset();
-
-    }
+//    private void respawnEntities(CollisionData data, int entity){
+//        //TODO: Make this method much more general
+//        PositionComp posComp = (PositionComp) worldContainer.getComponent(entity, PositionComp.class);
+//
+//        posComp.setPos(new Vec2(300,450));
+//
+//        ((DamageableComp)worldContainer.getComponent(entity, DamageableComp.class)).reset();
+//
+//    }
 
 }
