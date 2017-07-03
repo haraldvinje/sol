4dc4fe5edb221f8c47b2cf41a7f189a57ffd7f41
Eirik Skjærseth <eirik1502@gmail.com>
2017-07-03 17:18:35 +0200

    define view matrix

--- src/main/java/engine/graphics/RenderSys.java
+++ src/main/java/engine/graphics/RenderSys.java
@@ -24,7 +24,9 @@ public class RenderSys implements Sys {
 
     private WorldContainer wc;
 
-    Mat4 projectionTransform = Mat4.orthographic(0, 1600, 900, 0, -10, 10);
+    private Mat4 viewTransform = Mat4.translate(new Vec3(0, 0,0));
+
+    private Mat4 projectionTransform = Mat4.orthographic(0, 1600, 900, 0, -10, 10);
 
 
     public RenderSys(Window window) {
@@ -76,7 +78,7 @@ public class RenderSys implements Sys {
             Mat4 modelTransform = modelTranslate.multiply(modelScale.multiply(modelRotate.multiply( modelCenterTranslate )));
 
             ColoredMeshComp coloredMeshComp = (ColoredMeshComp)wc.getComponent(entity, ColoredMeshComp.class);
-            renderColoredMesh(coloredMeshComp.getMesh(), modelTransform, Mat4.identity(), projectionTransform);
+            renderColoredMesh(coloredMeshComp.getMesh(), modelTransform, viewTransform, projectionTransform);
 
 
 
@@ -106,7 +108,7 @@ public class RenderSys implements Sys {
 
 
             TexturedMeshComp texturedMeshComp = (TexturedMeshComp)wc.getComponent(entity, TexturedMeshComp.class);
-            renderTexturedMesh(texturedMeshComp.getMesh(), modelTransform, Mat4.identity(), projectionTransform);
+            renderTexturedMesh(texturedMeshComp.getMesh(), modelTransform, viewTransform, projectionTransform);
 
 
         }
