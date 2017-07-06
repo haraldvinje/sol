0f8c70f7930ba1407c3a6289a0418166fac4c46c
Eirik <eirik1502@gmail.com>
2017-07-06 16:02:40 +0200

    split Client network system. Add a comp to determine who is controlled by the client

---
+++ src/main/java/engine/network/client/ClientControlledComp.java
@@ -0,0 +1,13 @@
+package engine.network.client;
+
+import engine.Component;
+import engine.network.CharacterInputData;
+
+/**
+ * Created by eirik on 05.07.2017.
+ */
+public class ClientControlledComp implements Component {
+
+
+
+}
