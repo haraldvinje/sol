3f5e6001da919b488034df60f13653153987c9cf
Eirik <eirik1502@gmail.com>
2017-07-06 15:55:30 +0200

    User input is now stored in UserCharacterInputComp, and a new system loads the data into character input if relevant

---
+++ src/main/java/engine/UserCharacterInputSys.java
@@ -0,0 +1,60 @@
+package engine;
+
+import engine.Sys;
+import engine.UserCharacterInputComp;
+import engine.UserInput;
+import engine.WorldContainer;
+import engine.character.CharacterInputComp;
+import engine.network.CharacterInputData;
+
+/**
+ * Created by eirik on 15.06.2017.
+ */
+public class UserCharacterInputSys implements Sys {
+
+
+    private WorldContainer wc;
+
+    private UserInput userInput;
+
+
+    public UserCharacterInputSys(UserInput userInput) {
+        this.userInput = userInput;
+    }
+
+    @Override
+    public void setWorldContainer(WorldContainer wc) {
+        this.wc = wc;
+    }
+
+    @Override
+    public void update() {
+
+        for (int entity : wc.getEntitiesWithComponentType(UserCharacterInputComp.class)) {
+
+            UserCharacterInputComp inputComp = (UserCharacterInputComp)wc.getComponent(entity, UserCharacterInputComp.class);
+
+            CharacterInputData inData = new CharacterInputData();
+
+            inData.setMoveLeft(userInput.isKeyboardPressed(UserInput.KEY_A));
+            inData.setMoveRight(userInput.isKeyboardPressed(UserInput.KEY_D));
+            inData.setMoveUp(userInput.isKeyboardPressed(UserInput.KEY_W));
+            inData.setMoveDown(userInput.isKeyboardPressed(UserInput.KEY_S));
+
+            inData.setAction1(userInput.isMousePressed(UserInput.MOUSE_BUTTON_1));
+            inData.setAction2(userInput.isMousePressed(UserInput.MOUSE_BUTTON_2));
+            inData.setAction3(userInput.isKeyboardPressed(UserInput.KEY_SPACE));
+
+            inData.setAimX(userInput.getMouseX());
+            inData.setAimY(userInput.getMouseY());
+
+            inputComp.setClientData(inData);
+
+        }
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+}
