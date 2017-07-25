fa2dc20745f771b9cf3661bb2a489c51d8cf8701
haraldvinje <haraldvinje@gmai.com>
2017-07-25 09:51:08 +0200

    minor changes, cleanup and splitting up GameUtils into more classes to keep it cleaner

--- src/main/java/engine/network/server/ServerClientHandler.java
+++ src/main/java/engine/network/server/ServerClientHandler.java
@@ -6,6 +6,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.LinkedList;
 
 /**
