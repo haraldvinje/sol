cfc2f8c6dcdbf689a677fd6e1254d6c40c42cd5e
haraldvinje <haraldvinje@gmai.com>
2017-07-27 16:16:55 +0200

    first version of audio system

--- src/main/java/engine/audio/AudioSys.java
+++ src/main/java/engine/audio/AudioSys.java
@@ -1,7 +1,52 @@
 package engine.audio;
 
+import engine.PositionComp;
+import engine.Sys;
+import engine.WorldContainer;
+import javafx.geometry.Pos;
+
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.ALC10.alcCloseDevice;
+import static org.lwjgl.openal.ALC10.alcDestroyContext;
+
 /**
  * Created by haraldvinje on 27-Jul-17.
  */
-public class AudioSys {
+public class AudioSys implements Sys {
+
+
+    private WorldContainer wc;
+
+    @Override
+    public void setWorldContainer(WorldContainer wc) {
+        this.wc = wc;
+    }
+
+    @Override
+    public void update() {
+        wc.entitiesOfComponentTypeStream(AudioComp.class).forEach(entity-> {
+            AudioComp ac = (AudioComp) wc.getComponent(entity, AudioComp.class);
+            PositionComp posComp = (PositionComp) wc.getComponent(entity, PositionComp.class);
+
+            if (ac.requestSound!=-1){
+                ac.setPosition(posComp.getPos3());
+                ac.playSound(ac.requestSound);
+            }
+
+            //resetting so sound does not play repeatedly
+            ac.requestSound = -1;
+        });
+
+        wc.entitiesOfComponentTypeStream(SoundListenerComp.class).forEach(entity-> {
+            PositionComp posComp = (PositionComp) wc.getComponent(entity, PositionComp.class);
+            System.out.println("Position for listener: " + posComp.getPos());
+            alListener3f(AL_POSITION, posComp.getX(), posComp.getY(), posComp.getZ());
+        });
+    }
+
+
+    @Override
+    public void terminate() {
+        AudioMaster.terminate();
+    }
 }
